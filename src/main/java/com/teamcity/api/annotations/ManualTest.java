eb205caac8b1c5ea77253089c602077ebdacf8fc
alex.pshe <alexpshe.help@gmail.com>
2025-08-25 17:56:31 -0400

    Fix Checkstyle violation: Move Javadoc comment before annotation declaration

--- src/main/java/com/teamcity/api/annotations/ManualTest.java
+++ src/main/java/com/teamcity/api/annotations/ManualTest.java
@@ -6,12 +6,12 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Target(METHOD)
-@Retention(RUNTIME)
 /**
  * Annotation for marking tests that are manual and don't have implemented steps.
  * These tests only contain Allure step descriptions and logic behind the steps.
  */
+@Target(METHOD)
+@Retention(RUNTIME)
 public @interface ManualTest {
     String value() default "";
 }
