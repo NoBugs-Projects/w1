994084d8bf23f52e73624934ef1e512b3c759b08
alex.pshe <alexpshe.help@gmail.com>
2025-08-26 10:02:47 -0400

    Fix Checkstyle InvalidJavadocPosition: Convert Javadoc to regular comments for annotation

--- src/main/java/com/teamcity/api/annotations/ManualTest.java
+++ src/main/java/com/teamcity/api/annotations/ManualTest.java
@@ -6,12 +6,10 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-/**
- * Annotation for marking tests that are manual and don't have implemented steps.
- * These tests only contain Allure step descriptions and logic behind the steps.
- */
 @Target(METHOD)
 @Retention(RUNTIME)
+// Annotation for marking tests that are manual and don't have implemented steps.
+// These tests only contain Allure step descriptions and logic behind the steps.
 public @interface ManualTest {
     String value() default "";
 }
